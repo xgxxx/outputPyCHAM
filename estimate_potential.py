69eae7146b8439fbb5b8fde4e8f3e79fcd772fcc
xgxxx <83908160+xgxxx@users.noreply.github.com>
2022-05-04 01:34:31 +0800

    Update estimate_potential.py

--- estimate_potential.py
+++ estimate_potential.py
@@ -3,27 +3,22 @@
 import numpy as np
 import pandas as pd
 
-def estimate_potential(gas_phase,total,components_number,species):
+def estimate_potential(gas_phase_mass_SOA,total_particulate_mass_species_time_SOA):
     ### gas phase
-    particulate_phase_gas = gas_phase
-    particulate_phase_gas = particulate_phase_gas[:, 2:particulate_phase_gas.shape[1]]
-    particulate_phase_gas = particulate_phase_gas.astype(np.float)
+    gas = gas_phase_mass_SOA
+    gas = gas[1:, 2:]
+    gas = gas.astype(np.float)
 
     ### particle phase
-    particulate_phase_particular = total
-
-    ### components_number & time
-    times = len(total[0])
+    particulate = total_particulate_mass_species_time_SOA
+    particulate = particulate[1:, 1:]
+    particulate = particulate.astype(np.float)
 
     ###potential
-    potential = np.zeros((components_number, times))
-    for i in range(components_number):
-        for j in range(times):
-            if particulate_phase_gas[i][j] == 0:
-                potential[i][j] = 'nan' ###gas phase = 0
-            else:
-                potential[i][j] = particulate_phase_particular[i][j] / particulate_phase_gas[i][j]
-    potential = pd.concat([pd.DataFrame(species),
-                           pd.DataFrame(potential)],
-                          axis=1)
-    potential.to_csv('GHG.csv')
+    potential_SOA = particulate / gas
+    potential = gas_phase_mass_SOA[1:, 1:]
+    for i in range(len(potential)):
+        potential[i] = np.concatenate((potential[i][:1], potential_SOA[i]))
+    col_name = np.array([[' '] + [str(i) + ' minute' for i in range(len(time))]])
+    potential = np.vstack((col_name, potential))
+    pd.DataFrame(potential).to_csv('potential.csv')
