375d3bf6ab6981e14cb614198ba457274f4f5d14
xgxxx <83908160+xgxxx@users.noreply.github.com>
2022-05-03 20:44:29 +0800

    Update PyCHAM Post processing_final.py

--- PyCHAM Post processing_final.py
+++ PyCHAM Post processing_final.py
@@ -244,7 +244,7 @@ for i in range(len(tracked_comp)):
     temperature_new = np.tile(temperature[0], (len(rate_of_change) - 1, len(rate_of_change[0])))
     rate_of_change_mass = rate_of_change_ppb
     rate_of_change_mass[1:] = rate_of_change_mass[1:] * 12.187 / temperature_new * tracked_comp_weight[0]
-    pd.DataFrame(rate_of_change_mass).to_csv(tracked_comp[i]+"_rate_of_change (\u03BCg.m\u207B\u00b3).csv")
+    pd.DataFrame(rate_of_change_mass).to_csv(tracked_comp[i]+"_rate_of_change (\u03BCg.m\u207B\u00b3.s\u207B\u00b9).csv")
 
 ##### total_concentration_of_injected_components (mass concentration, unit: ug/m3)
 total_concentration_of_injected_components_file_path = r"total_concentration_of_injected_components"
